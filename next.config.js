b6007ddc9542ae5155bb5805e664f852f9c42df6
gkd248 <gdarnell@utexas.edu>
2025-08-10 14:15:37 -0500

    Add Pluralsight cert, update Next.js configto ignore eslint rules during build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,  // This will ignore ESLint errors during build
+  },
 }
 
 export default nextConfig
